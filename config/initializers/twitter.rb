ee605bb06ce9d6a87ce4843413f0b489430ea2ea
CodingPeaches90 <jordanmay10@hotmail.com>
2019-03-29 00:40:09 +0000

    final commit

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,3 +1,4 @@
+# Reference : https://stackoverflow.com/questions/40023816/how-to-call-twitter-gem-methods-on-the-twitter-api-client-in-the-view-file
 $client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV['CONSUMER_KEY']
   config.consumer_secret     = ENV['CONSUMER_SECRET']
